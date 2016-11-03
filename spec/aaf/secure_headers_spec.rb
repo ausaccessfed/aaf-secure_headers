84f0343dd1fcdb1bc8303e98bc4aa0dcce48bc5b
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-11-03 15:38:52 +1000

    Configure RSpec with more aggressive defaults

--- spec/aaf/secure_headers_spec.rb
+++ spec/aaf/secure_headers_spec.rb
@@ -1,6 +1,4 @@
-require 'spec_helper'
-
-describe AAF::SecureHeaders do
+RSpec.describe AAF::SecureHeaders do
   it 'has a version number' do
     expect(AAF::SecureHeaders::VERSION).not_to be nil
   end
