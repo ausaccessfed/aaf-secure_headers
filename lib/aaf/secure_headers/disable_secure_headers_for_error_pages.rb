211559ecf7e3f861b540533305c724784125f367
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-11-03 15:48:10 +1000

    Disable secure headers for error responses
    This fixes the behaviour of Rails' default error handlers for
    development mode, which require inline styles and javascript.

--- lib/aaf/secure_headers/disable_secure_headers_for_error_pages.rb
+++ lib/aaf/secure_headers/disable_secure_headers_for_error_pages.rb
@@ -3,6 +3,17 @@
 module AAF
   module SecureHeaders
     class DisableSecureHeadersForErrorPages
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        @app.call(env).tap do |(status, _, _)|
+          next if status < 400
+          request = ActionDispatch::Request.new(env)
+          ::SecureHeaders.opt_out_of_all_protection(request)
+        end
+      end
     end
   end
 end
